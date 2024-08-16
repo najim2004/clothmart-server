d7307a518eca7eac3a8634be4d47cc53cc15bacc
najim2004 <najim10112004@gmail.com>
2024-08-16 22:58:38 +0600

    updated productController

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,76 +1,52 @@
 exports.getProducts = async (req, res) => {
   try {
     const db = req.app.locals.db;
-    const {
-      page = 1,
-      limit = 12,
-      search = "",
-      filters = {},
-      sort = "price-asc",
-    } = req.query;
+    const { page = 1, limit = 12, search = "", filters = {} } = req.query;
+    const { brand, category, priceRange, sort = "date-desc" } = filters;
+
     const skip = (page - 1) * limit;
     const query = {};
 
     // Apply search query
     if (search) {
-      query.productName = { $regex: search, $options: "i" }; // Case-insensitive search
+      query.productName = { $regex: search, $options: "i" };
     }
 
     // Apply filters
-    if (filters.category) {
-      query.category = filters.category;
+    if (category) {
+      query.category = category;
     }
-    if (filters.brand) {
-      query.brand = filters.brand;
+    if (brand) {
+      query.brand = brand;
     }
-    if (filters.priceRange[0] || filters.priceRange[1]) {
+    if (priceRange[0] || priceRange[1]) {
       query.price = {};
-      if (filters.priceRange[0])
-        query.price.$gte = parseFloat(filters.priceRange[0]);
-      if (filters.priceRange[1])
-        query.price.$lte = parseFloat(filters.priceRange[1]);
+      if (priceRange[0]) query.price.$gte = parseFloat(priceRange[0]);
+      if (priceRange[1]) query.price.$lte = parseFloat(priceRange[1]);
     }
-
     // Determine sorting
-    let sortOption;
-    switch (filters.sort) {
-      case "price-asc":
-        sortOption = { price: 1 };
-        break;
-      case "price-desc":
-        sortOption = { price: -1 };
-        break;
-      case "date-asc":
-        sortOption = { creationDate: 1 };
-        break;
-      case "date-desc":
-        sortOption = { creationDate: -1 };
-        break;
-      default:
-        sortOption = { creationDate: -1 };
-    }
+    const sortOption =
+      sort === "price-asc"
+        ? { price: 1 }
+        : sort === "price-desc"
+        ? { price: -1 }
+        : sort === "date-asc"
+        ? { creationDate: 1 }
+        : { creationDate: -1 };
 
-    // console.log(filters);
-    // console.log(query);
-
-    // Get total count of products
+    // Fetch products with pagination and sorting
     const totalProducts = await db
       .collection("allproducts")
       .countDocuments(query);
-
-    // Fetch paginated and sorted products
     const products = await db
       .collection("allproducts")
       .find(query)
-      .skip(parseInt(skip))
+      .skip(skip)
       .limit(parseInt(limit))
       .sort(sortOption)
       .toArray();
 
-    res.status(200).json({
-      totalProducts,
-      products,
-    });
+    res.status(200).send({ totalProducts, products });
   } catch (error) {
     res.status(500).send({ message: "Error fetching products", error });
   }
