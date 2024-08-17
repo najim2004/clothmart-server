634c34096366b63d10e3c636c6b1d76401285e19
najim2004 <najim10112004@gmail.com>
2024-08-17 14:13:38 +0600

    maked brandNameController

---
+++ controllers/brandNameController.js
@@ -0,0 +1,13 @@
+exports.getBrandName = async (req, res) => {
+  try {
+    const db = req.app.locals.db;
+    const brand = await db.collection("allbrands").find().toArray();
+    if (!brand) {
+      return res.status(404).send({ msg: "Brand not found" });
+    }
+    res.send(brand);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send({ msg: "Server error", err });
+  }
+};
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,8 +1,5 @@
 const express = require("express");
-const {
-  getProducts,
-  createProduct,
-} = require("../controllers/productController");
+const { getProducts } = require("../controllers/productController");
 const router = express.Router();
 router.get("/", getProducts);
 
